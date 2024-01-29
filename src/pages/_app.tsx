73ee85303b8dcaaf39d631c494fba373f06e683c
benjichat <63160791+benjichat@users.noreply.github.com>
2024-01-29 21:03:05 +0100

    try to extract username

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,21 +6,29 @@ import { useEffect, useState } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [isHashValid, setIsHashValid] = useState(false);
+  const [username, setUsername] = useState(null);
 
   // Wait for validation to complete before rendering the page and stop the
   // rendering if the hash is invalid. Comment out the following useEffect
   // hook to see the page render without the hash validation.
+  // I will receive ID, name, username, language_code, photo
   useEffect(() => {
-    axios
-      .post('/api/validate-hash', { hash: window.Telegram.WebApp.initData })
-      .then((response) => setIsHashValid(response.status === 200));
+    axios.post('/api/validate-hash', { hash: window.Telegram.WebApp.initData }).then((response) => {
+      setIsHashValid(response.status === 200);
+      if (response.status === 200) {
+        const { username } = response.data.username; // Assuming the user ID is returned in the response
+        setUsername(username);
+      }
+    });
   }, []);
 
   if (!isHashValid) {
     return null;
   }
 
-  return <Component {...pageProps} />;
+  const modifiedPageProps = { ...pageProps, username };
+
+  return <Component {...modifiedPageProps} />;
 }
 
 export default MyApp;
