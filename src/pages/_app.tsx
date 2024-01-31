da3d7e496949b2d0874cb40b126f22c274cc26be
benjichat <63160791+benjichat@users.noreply.github.com>
2024-01-31 11:08:04 +0100

    Revert "adds new WebAppUser Type"
    This reverts commit be50079325d242c23920c9a93023a2dd27bef18e.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,36 +4,14 @@ import axios from 'axios';
 import type { AppProps } from 'next/app';
 import { useEffect, useState } from 'react';
 
-interface WebAppUserNew {
-  /**
-   * A unique identifier for the user or bot.
-   */
-  id?: number;
-  /**
-   * True, if this user is a bot. Returns in the receiver field only.
-   */
-  is_bot: boolean;
-  /**
-   * First name of the user or bot.
-   */
+type TelegramUserData = {
+  id: number;
   first_name: string;
-  /**
-   * Last name of the user or bot.
-   */
   last_name?: string;
-  /**
-   * Username of the user or bot.
-   */
   username?: string;
-  /**
-   * IETF language tag of the user's language. Returns in user field only.
-   */
   language_code?: string;
-  /**
-   * URL of the user’s profile photo. The photo can be in .jpeg or .svg formats. Only returned for Web Apps launched from the attachment menu.
-   */
   photo_url?: string;
-}
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [isHashValid, setIsHashValid] = useState(false);
@@ -45,11 +23,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       if (response.status === 200) {
         // After hash validation, retrieve user data from initDataUnsafe
         if (window.Telegram && window.Telegram.WebApp && window.Telegram.WebApp.initDataUnsafe) {
-          const webAppUserData: WebAppUserNew | undefined =
-            window.Telegram.WebApp.initDataUnsafe.user;
-          if (webAppUserData) {
-            // Rest of the code
-          }
+          const webAppUserData = window.Telegram.WebApp.initDataUnsafe.user;
           if (webAppUserData) {
             if (webAppUserData.username && webAppUserData.username !== null) {
               setUsername(webAppUserData.username);
